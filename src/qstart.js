c09604ead103980ab9a89aeb61d930b2ee2b4dbc
Gaëtan Renaudeau <gre@zenexity.com>
2013-07-05 14:40:24 +0200

    Make Qstart a Promise instead of a function returning a Promise. + There is only one "readystatechange complete" event on a browser so it can be represent by one Promise. + This event should not be missed so we listen to it directy.

--- src/qstart.js
+++ src/qstart.js
@@ -10,19 +10,17 @@
     }
 
 })(function () {
-    return function () {
-        var Q = window.Q || require('q'),
-            d = Q.defer(),
-            successƒ = function () {
-                window.removeEventListener("error", errorƒ);
-                d.resolve(window.document);
-            },
-            errorƒ = function (err) { d.reject(err); };
+    var Q = window.Q || require('q'),
+        d = Q.defer(),
+        successƒ = function () {
+            window.removeEventListener("error", errorƒ);
+            d.resolve(window.document);
+        },
+        errorƒ = function (err) { d.reject(err); };
 
-        window.document.addEventListener("readystatechange", function () {
-            if (document.readyState == "complete") successƒ();
-        }, false);
-        window.addEventListener("error", errorƒ, false);
-        return d.promise;
-    };
+    window.document.addEventListener("readystatechange", function () {
+        if (document.readyState == "complete") successƒ();
+    }, false);
+    window.addEventListener("error", errorƒ, false);
+    return d.promise;
 });
